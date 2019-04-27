d4d33e5740c7e7f4bf01b7202eaa8ea0070ab4a2
Yusuke Taruya <talgorhythm@gmail.com>
2019-04-27 18:13:11 +0900

    into jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
-function App() {
-  return <div><h1>Hello, world!</h1></div>;
+
+class App extends React.Component{
+  render(){
+    const greeting = 'Hi, ';
+    const dom = <h1 className=''>{greeting}hello react</h1>
+    return (
+      <div>
+        <h1>{dom}</h1>
+        <input type='text' 
+          onClick={()=>{console.log('clicked')}}
+        />
+        <label htmlFor='bar'>bar
+        </label>
+      </div>
+    )
+  }
 }
+// function App() {
+//   const dom = <h1>Hello, World</h1>;
+//   return dom;
+// }
+/*
+class App extends Component{
+  rener() {
+    return React.createElement(
+      'div',
+      null,
+      'hello world'
+    )
+  }
+}
+}
+ */
 
 export default App;
+
