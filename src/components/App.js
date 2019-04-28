dbd75392a333488344eb3cc4ce48dacfb43a6b58
Yusuke Taruya <talgorhythm@gmail.com>
2019-04-28 20:53:59 +0900

    implement component with connect funciton

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const App = () => (<Counter></Counter>)
+import { increment, decrement} from '../actions';
 
-class Counter extends Component {
-  constructor(props){
-    super(props)
-    this.state = { count: 0}
-  };
-
-  handleAddition = () => {
-    this.setState({count: this.state.count + 1})
-  };
-
-  handleSubtraction = () => {
-    console.log('minus called')
-    this.setState({count: this.state.count - 1})
-  };
+class App extends Component {
   render(){
+    const props = this.props;
     return(
       <React.Fragment>
-        <div>count: {this.state.count}</div>
-        <button onClick={this.handleAddition}>
+        <div>value: {props.value}</div>
+        <button onClick={props.increment}>
         +
         </button>
-        <button onClick={this.handleSubtraction}>
+        <button onClick={props.decrement}>
         -
         </button>
       </React.Fragment>
@@ -31,5 +20,33 @@ class Counter extends Component {
   }
 }
 
-export default App;
+/*
+:stateの情報からコンポーネントに必要なものを取り出して、
+コンポーネント内のpropsとしてマッピングする
+
+@param state 状態のトップレベル 
+@return {Object} どういったオブジェクトをpropsとして対応させるか
+*/
+const mapStateToProps = state => ({ value: state.count.value })
+
+/*
+:あるアクションが発生した時に、reducerに、typeに応じた状態遷移を発生させる関数
+
+@param
+@return {Object}
+ */ 
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement()),
+})
+
+/* mapDispatchToProps関数のショートハンド
+ const mapdispatchToProps = ({increment, decrement});
+*/
+
+/*
+connect関数
+:stateとactionをcomponentに関連づける
+*/
+export default connect(mapStateToProps, mapDispatchToProps)(App)
 
